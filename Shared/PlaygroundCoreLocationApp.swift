237b72b332d062422acbd51783f8a73a24405be4
Nicole Liu <nicoleliu_@outlook.com>
2021-03-24 23:49:23 -0400

    Initial Commit

---
+++ Shared/PlaygroundCoreLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundCoreLocationApp.swift
+//  Shared
+//
+//  Created by Liu, Bojun on 2021-03-24.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundCoreLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
